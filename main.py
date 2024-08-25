39d39384bb52b0c78a37ec44f30f8b3a98a342fc
Butterroach <butterroach@proton.me>
2024-08-25 05:23:00 +0300

    almost forgot the license

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+"""
+    MochaPass: a CLI local password manager
+    Copyright (C) 2024  Butterroach
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 import argparse
 import base64
 import bcrypt
@@ -329,4 +347,5 @@ else:
     {tercol.hexa(0xd57962,tercol.bold("mocha"))}{tercol.yellow("pass")} - v{__version__}
         """
     )
-    print("Run --help for more info.")
+    print("Run --help for more info on how to use MochaPass.")
+    print("MochaPass is licensed under the GNU GPL-v3.")
